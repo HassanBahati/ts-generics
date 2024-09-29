14123539dec61288d85601ebfeb981f7067993d2
HassanBahati <mukisabahati@gmail.com>
2024-09-29 11:26:48 +0300

    feat: add basic generics

--- src/basic-generics.ts
+++ src/basic-generics.ts
@@ -4,3 +4,9 @@ function identity<T>(arg: T): T {
 
 console.log(identity(42));
 console.log(identity("Hello, TypeScript Generics!"));
+
+// works with any data type and you can identity the type later when the function is called
+
+// T is the variable ie when T is string, arg is of type string and return type is string
+
+// <T> is a generic parameter - acts as a placeholder for any type
