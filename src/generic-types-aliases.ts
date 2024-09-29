809fb927ec06dfa12312139758473f7063f2dd57
HassanBahati <mukisabahati@gmail.com>
2024-09-29 12:11:40 +0300

    feat: add generic types aliases

---
+++ src/generic-types-aliases.ts
@@ -0,0 +1,36 @@
+type Pair<T, U> = [T, U];
+
+let examplePair: Pair<number, number> = [10, 20];
+let exampleName: Pair<string, string> = ["john", "doe"];
+
+console.log(examplePair);
+console.log(exampleName);
+
+type User<
+  T = string,
+  U = number,
+  X extends Pair<any, any> = Pair<number, number>
+> = {
+  name: T;
+  age: U;
+  role: T;
+  balance: X;
+};
+
+let basicUser: User = {
+  name: "John Doe",
+  age: 30,
+  role: "user",
+  balance: [10, 40],
+};
+
+// the initial type can be overridden
+let overriddenUser: User<string, string> = {
+  name: "John Doe",
+  age: "30",
+  role: "user",
+  balance: [10, 40],
+};
+
+console.log(basicUser);
+console.log(overriddenUser);
